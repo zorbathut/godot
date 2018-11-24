0cc7f95355dacf6aca82bb361ec88372cee77d99
Ben Rog-Wilhelm <zorba-githubdesktop20180420@pavlovian.net>
2018-11-23 22:08:38 -0800

    Failed attempt at the animation changes

--- monobuild.bat
+++ monobuild.bat
@@ -1,4 +1,10 @@
 call scons p=windows tools=yes module_mono_enabled=yes mono_glue=no -j12
-start /w .\bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+.\bin\godot.windows.tools.64.mono.exe --generate-mono-glue modules/mono/glue
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 call scons p=windows module_mono_enabled=yes -j12
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 .\bin\godot.windows.tools.64.mono.exe c:\werk\shadebinder\project.godot
