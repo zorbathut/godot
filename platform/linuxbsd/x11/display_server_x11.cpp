4830c000d7101a713aa087154f462825cb80dcfd
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-05-22 09:48:46 -0500

    Fix: Accidentally introduced double-endif.

--- platform/linuxbsd/x11/display_server_x11.cpp
+++ platform/linuxbsd/x11/display_server_x11.cpp
@@ -6830,7 +6830,7 @@ DisplayServerX11::DisplayServerX11(const String &p_rendering_driver, WindowMode 
 		driver_found = true;
 	}
 #endif
-#endif
+
 	// Initialize context and rendering device.
 #if defined(GLES3_ENABLED)
 	if (rendering_driver == "opengl3" || rendering_driver == "opengl3_es") {
