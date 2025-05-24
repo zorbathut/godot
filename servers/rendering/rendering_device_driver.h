e44dcdb10f4e1b26c0d74ab372b69fc9d9abcad0
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-05-24 02:17:53 -0500

    remove that stub

--- servers/rendering/rendering_device_driver.h
+++ servers/rendering/rendering_device_driver.h
@@ -361,9 +361,6 @@ public:
 		BARRIER_ACCESS_STORAGE_CLEAR_BIT = (1 << 27),
 	};
 
-	#ifdef MemoryBarrier
-	#error no stop
-	#endif
 	struct MemoryBarrier {
 		BitField<BarrierAccessBits> src_access;
 		BitField<BarrierAccessBits> dst_access;
