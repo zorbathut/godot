70c7225ddd7dfe82d4391c66da35f7bde7bab946
Ben Rog-Wilhelm <zorba-github@pavlovian.net>
2025-09-11 02:46:15 -0500

    Remove the classdb deinit.

--- core/object/class_db.cpp
+++ core/object/class_db.cpp
@@ -840,7 +840,7 @@ use_script:
 	return scr.is_valid() && scr->is_valid() && scr->is_abstract();
 }
 
-void ClassDB::_add_class(const StringName &p_class, const StringName &p_inherits, void (*p_deinit_func)(bool deinit)) {
+void ClassDB::_add_class(const StringName &p_class, const StringName &p_inherits) {
 	Locker::Lock lock(Locker::STATE_WRITE);
 
 	const StringName &name = p_class;
@@ -850,7 +850,6 @@ void ClassDB::_add_class(const StringName &p_class, const StringName &p_inherits
 	classes[name] = ClassInfo();
 	ClassInfo &ti = classes[name];
 	ti.name = name;
-	ti.deinit_func = p_deinit_func;
 	ti.inherits = p_inherits;
 	ti.api = current_api;
 
